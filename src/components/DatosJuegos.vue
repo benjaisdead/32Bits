5f5eca24d0379478abc7cbd03db71ba7bf14a627
benjaisdead <40874757+benjaisdead@users.noreply.github.com>
2022-07-04 17:37:17 -0400

    Desafio

---
+++ src/components/DatosJuegos.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <ul>
+        <li v-for="dato in juegos">
+        {{ dato.nombre }}
+        </li>
+    </ul>
+
+          <div id="laTabla">
+                  <table border="1">
+                    <tr>
+                      <th>Codigo</th>
+                      <th>Stock</th>
+                      <th>Nombre</th>
+                      <th>Precio</th>
+                    </tr>
+                    <tr v-for="registro in juegos" v-bind:style="`color: ${registro.color}`">
+                      <td>{{ registro.codigo}}</td>
+                      <td>{{ registro.nombre}}</td>
+                      <td>{{ registro.stock}}</td>
+                      <td>{{ registro.precio}}</td>
+                    </tr>
+                  </table>
+          </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+export default {
+  name: 'DatosJuegos',
+  props: {
+    msg: String
+  },
+  computed:{
+    ...mapState(["juegos"]),
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+
+#laTabla{
+  text-align:center;
+}
+</style>
---
+++ src/store/index.js
@@ -0,0 +1,19 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import juegos from '@/juegos.json'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    juegos
+  },
+  getters: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
